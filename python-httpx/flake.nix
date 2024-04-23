f42cfba4e1106cc46a8457680157b0489bc2f509
paveun <a.tresierrajansen@gmail.com>
2024-04-23 10:11:51 -0400

    add python-httpx

---
+++ python-httpx/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Python httpx requests dev template";
+
+  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        venvDir = "venv";
+        packages = with pkgs;
+          [python311 postman]
+          ++ (with pkgs.python311Packages; [
+            pip
+            httpx
+            venvShellHook
+          ]);
+      };
+    });
+  };
+}
