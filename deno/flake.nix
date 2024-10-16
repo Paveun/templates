c8adf5294bcad2198ba9df0ffece118cde72d4c2
paveun <a.tresierrajansen@gmail.com>
2024-10-16 10:19:45 -0400

    add deno

---
+++ deno/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }@inputs:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.deno
+
+            # You can set the major version of Node.js to a specific one instead
+            # of the default version
+            # pkgs.nodejs-22_x
+
+            # It is possible to use bun instead of node.
+            # pkgs.bun
+
+            # Optionally, you can add yarn or pnpm for package management for node.
+            # pkgs.nodePackages.pnpm
+            # pkgs.yarn
+
+            # pkgs.nodePackages.typescript
+            # pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
