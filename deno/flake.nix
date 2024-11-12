eb2e91e59d36eb218f5493a16c63873ee1cdf12e
paveun <a.tresierrajansen@gmail.com>
2024-11-11 20:21:18 -0500

    formatting

--- deno/flake.nix
+++ deno/flake.nix
@@ -6,18 +6,19 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs =
-    { systems, nixpkgs, ... }@inputs:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            deno
-          ];
-        };
-      });
-    };
+  outputs = {
+    systems,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          deno
+        ];
+      };
+    });
+  };
 }
